67f500e97f45fcbbccb60ef21ceb82ee8849faa7
rockclimber147 <daylencsmith@gmail.com>
2023-11-30 18:58:38 -0800

    Add parameter object validation

--- frontend/unicorns.js
+++ frontend/unicorns.js
@@ -1,3 +1,7 @@
+/**
+ * Gets the values of the checkboxes and constructs an object with the values
+ * @returns Parameter object
+ */
 function getUnicornDisplayParameters(){
     let checkboxes = document.getElementById("unicornAttributeCheckboxes").getElementsByTagName("input");
     let parameters = {};
@@ -5,4 +9,21 @@ function getUnicornDisplayParameters(){
         parameters[checkboxes[i].id.replace('flexCheck', '')] = checkboxes[i].checked;
     }
     return parameters;
+}
+
+
+/**
+ * Validates the parameters to ensure at least one is true
+ * @param {Object} parameters The parameters to display the unicorns with
+ * @returns True if the parameters are valid, false otherwise
+ */
+function validateUnicornDisplayParameters(parameters){
+    let valid = false;
+    for(let key in parameters){
+        if(parameters[key] === true){
+            valid = true;
+            break;
+        }
+    }
+    return valid;
 }
