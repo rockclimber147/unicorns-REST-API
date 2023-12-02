3f605c871e543965519e4ed01b699befddc9d629
Daylen Smith <daylencsmith@gmail.com>
2023-12-01 21:44:22 -0800

    Add try catch to server.js

--- backend/server.js
+++ backend/server.js
@@ -16,15 +16,11 @@ app.listen(3000, () => {
     console.log('listening on port 3000')
 })
 
-async function getUnicorns(name) {
-    return await unicorn.find({ $or: [{ name: name }, { weight: { $lt: 1700} }] })
-}
-
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/unicorns', async (req, res) => {
+    try {
     console.log('request received!:', req.query)
-
     let displayObject = generateDisplayObject(req.query.display)
     delete req.query.display;
 
@@ -34,43 +30,58 @@ app.get('/unicorns', async (req, res) => {
     result = await unicorn.find(queryObject, displayObject);
     // console.log(result)
     res.json(result)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err)
+    }
 })
 
 function generateDisplayObject(displayString) {
-    let displayObject = {_id: 0}
-    let displayArray = displayString.split(',')
-    for (const key of displayArray) {
-        displayObject[key] = 1
-    }
-    return displayObject
-} 
-
-function generateQueryObject(incomingQuery){
-    // Make an empty query object
-    let queryParams = [];
-    let queryObject = {};
-    // Add and/or to the query object
-    queryObject[incomingQuery.fieldRelationType] = queryParams;
-    // Remove the fieldRelationType from the incoming query so it's not processed
-    delete incomingQuery.fieldRelationType;
-    // Iterate over the query parameters
-    for (const key in incomingQuery) {
-        let value = incomingQuery[key]
-        console.log('k:v', key, value) 
-        // handle the case where the value is a relational query (weight or vampires)
-        if (key.includes('_')) {
-            let [field, relation] = key.split('_')
-            queryParams.push({ [`${field}`]: { [`${relation}`]: incomingQuery[key] }})
-        // handle the case where an array of string is given (loves or name)
-        } else if (key === 'loves' || key === 'name') {
-            queryParams.push({ [`${key}`]: { $in: incomingQuery[key].split(',') }})
-        } else {
-            queryParams.push({ [`${key}`]: incomingQuery[key]})
+    try {
+        // id is disabled by default
+        let displayObject = { _id: 0 }
+        let displayArray = displayString.split(',')
+        for (const key of displayArray) {
+            displayObject[key] = 1
         }
+        return displayObject
+    } catch (err) {
+        console.log(err)
+        throw new Error('Invalid display parameters!')
     }
-    // Return an empty object if no query parameters are given
-    if (queryParams.length === 0) {
-        queryObject = {}
+}
+
+    function generateQueryObject(incomingQuery) {
+        try{
+        // Make an empty query object
+        let queryParams = [];
+        let queryObject = {};
+        // Add and/or to the query object
+        queryObject[incomingQuery.fieldRelationType] = queryParams;
+        // Remove the fieldRelationType from the incoming query so it's not processed
+        delete incomingQuery.fieldRelationType;
+        // Iterate over the query parameters
+        for (const key in incomingQuery) {
+            let value = incomingQuery[key]
+            console.log('k:v', key, value)
+            // handle the case where the value is a relational query (weight or vampires)
+            if (key.includes('_')) {
+                let [field, relation] = key.split('_')
+                queryParams.push({ [`${field}`]: { [`${relation}`]: incomingQuery[key] } })
+                // handle the case where an array of string is given (loves or name)
+            } else if (key === 'loves' || key === 'name') {
+                queryParams.push({ [`${key}`]: { $in: incomingQuery[key].split(',') } })
+            } else {
+                queryParams.push({ [`${key}`]: incomingQuery[key] })
+            }
+        }
+        // Return an empty object if no query parameters are given
+        if (queryParams.length === 0) {
+            queryObject = {}
+        }
+        return queryObject
+    } catch (err) {
+        console.log(err)
+        throw new Error('Invalid query parameters!')
     }
-    return queryObject
 }
